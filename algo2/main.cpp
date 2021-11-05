e688ec0f218716b8378d42615500051b034c62ab
Serhat Gundem <b2200356820@cs.hacettepe.edu.tr>
2021-11-05 17:34:17 +0300

    Initial Commit

---
+++ algo2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  algo2
+//
+//  Created by Serhat Gündem on 5.11.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
